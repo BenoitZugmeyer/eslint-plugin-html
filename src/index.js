583e1c124e6c84972805c30d7e41c15d092ad146
Benoît Zugmeyer <bzugmeyer@gmail.com>
2016-01-21 13:50:57 +0100

    Support indent setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,31 @@
 "use strict";
 
+var eslint = require("eslint");
 var extract = require("./extract");
+
+// Disclaimer:
+//
+// This is not a long term viable solution. ESLint needs to improve its processor API to
+// provide access to the configuration before actually preprocess files, but it's not
+// planed yet. This solution is quite ugly but shouldn't alter eslint process.
+//
+// Related github issues:
+// https://github.com/eslint/eslint/issues/3422
+// https://github.com/eslint/eslint/issues/4153
+
+var verify = eslint.linter.verify;
+
+function patch() {
+  eslint.linter.verify = function (textOrSourceCode, config, filenameOrOptions, saveState) {
+    var indentDescriptor = config.settings && config.settings["html/indent"];
+    currentInfos = extract(textOrSourceCode, indentDescriptor);
+    return verify.call(this, currentInfos.code, config, filenameOrOptions, saveState);
+  };
+}
+
+function unpatch() {
+  eslint.linter.verify = verify;
+}
 
 var currentInfos;
 var allowedExtensions = ["htm", "html", "xhtml", "vue", "hbs", "mustache"];
@@ -8,11 +33,13 @@ var allowedExtensions = ["htm", "html", "xhtml", "vue", "hbs", "mustache"];
 var htmlProcessor = {
 
   preprocess: function (content) {
-    currentInfos = extract(content);
-    return [currentInfos.code];
+    patch();
+    return [content];
   },
 
   postprocess: function (messages) {
+    unpatch();
+
     messages[0].forEach(function (message) {
       message.column += currentInfos.map[message.line] || 0;
     });
--- test/plugin.js
+++ test/plugin.js
@@ -3,14 +3,16 @@ var path = require("path");
 var CLIEngine = require("eslint").CLIEngine;
 var plugin = require("..");
 
-function execute(file, settings) {
+function execute(file, baseConfig) {
+  if (!baseConfig) baseConfig = {};
+
   var cli = new CLIEngine({
     extensions: ["html"],
     baseConfig: {
-      settings: settings,
-      rules: {
+      settings: baseConfig.settings,
+      rules: Object.assign({
         "no-console": 2,
-      },
+      }, baseConfig.rules),
     },
     ignore: false,
     useEslintrc: false,
@@ -47,6 +49,94 @@ describe("plugin", function () {
     assert.equal(messages[4].message, "Unexpected console statement.");
     assert.equal(messages[4].line, 28);
     assert.equal(messages[4].column, 13);
+  });
+
+  describe("html/indent setting", function () {
+    it("should automatically compute indent when nothing is specified", function () {
+      var messages = execute("indent-setting.html", {
+        rules: {
+          indent: [2, 2],
+        },
+      });
+
+      assert.equal(messages.length, 0);
+    });
+
+    it("should work with a zero absolute indentation descriptor", function () {
+      var messages = execute("indent-setting.html", {
+        rules: {
+          indent: [2, 2],
+        },
+
+        settings: {
+          "html/indent": 0,
+        },
+      });
+
+      assert.equal(messages.length, 3);
+
+      // Only the first script is correctly indented (aligned on the first column)
+
+      assert.equal(messages[0].message, "Expected indentation of 0 space characters but found 2.");
+      assert.equal(messages[0].line, 16);
+
+      assert.equal(messages[1].message, "Expected indentation of 0 space characters but found 6.");
+      assert.equal(messages[1].line, 22);
+
+      assert.equal(messages[2].message, "Expected indentation of 0 space characters but found 10.");
+      assert.equal(messages[2].line, 28);
+    });
+
+    it("should work with a non-zero absolute indentation descriptor", function () {
+      var messages = execute("indent-setting.html", {
+        rules: {
+          indent: [2, 2],
+        },
+
+        settings: {
+          "html/indent": 2,
+        },
+      });
+
+      assert.equal(messages.length, 3);
+
+      // The first script is incorrect since the second line gets dedented
+      assert.equal(messages[0].message, "Expected indentation of 2 space characters but found 0.");
+      assert.equal(messages[0].line, 11);
+
+      // The second script is correct.
+
+      assert.equal(messages[1].message, "Expected indentation of 0 space characters but found 4.");
+      assert.equal(messages[1].line, 22);
+
+      assert.equal(messages[2].message, "Expected indentation of 0 space characters but found 8.");
+      assert.equal(messages[2].line, 28);
+    });
+
+    it("should work with relative indentation descriptor", function () {
+      var messages = execute("indent-setting.html", {
+        rules: {
+          indent: [2, 2],
+        },
+
+        settings: {
+          "html/indent": "+2",
+        },
+      });
+
+      assert.equal(messages.length, 2);
+
+      // The first script is correct since it can't be dedented, but follows the indent
+      // rule anyway.
+
+      assert.equal(messages[0].message, "Expected indentation of 0 space characters but found 2.");
+      assert.equal(messages[0].line, 16);
+
+      // The third script is correct.
+
+      assert.equal(messages[1].message, "Expected indentation of 0 space characters but found 2.");
+      assert.equal(messages[1].line, 28);
+    });
   });
 
 });

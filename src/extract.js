84df755b5d2e03c7f753fc7202c7e8d4ffd67e5c
Benoît Zugmeyer <bzugmeyer@gmail.com>
2018-07-04 14:34:22 +0200

    code cleanup

--- src/extract.js
+++ src/extract.js
@@ -224,6 +224,7 @@ function extract(code, indentDescriptor, xmlMode, isJavaScriptMIMEType) {
   return {
     code: codeParts,
     badIndentationLines,
+    hasBOM: code.startsWith("\uFEFF"),
   }
 }
 

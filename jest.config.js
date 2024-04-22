c589d68601d2db44cede2be748ed09739fc1d3c9
Benoît Zugmeyer <bzugmeyer@gmail.com>
2024-04-22 14:52:21 +0200

    fix: drop jest (4/4): remove jest dependency

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-module.exports = {
-  moduleNameMapper: {
-    "@eslint/eslintrc/universal":
-      "<rootDir>/node_modules/@eslint/eslintrc/dist/eslintrc-universal.cjs",
-  },
-  prettierPath: null,
-  testPathIgnorePatterns: ["/node_modules/", "/fixtures/"],
-}
